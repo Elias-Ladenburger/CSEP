6499ce6d0d46b883d84a56324d382658207d38ed
Elias Ladenburger <elias@ladenburger-family.de>
2021-03-16 15:04:05 +0100

    Set up frontend for implementing wireframes

--- source/web/app.py
+++ source/web/app.py
@@ -6,4 +6,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", template_folder='templates')
+    return render_template("index.html")
+
+
+@app.route("/motivation")
+def motivation_page():
+    return render_template("motivation.html")
+
+@app.route("/approach")
+def approach_page():
+    return render_template("approach.html")
+
+@app.route("/concept")
+def concept_page():
+    return render_template("concept.html")
+
+@app.route("/scenarios")
+def scenarios_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+

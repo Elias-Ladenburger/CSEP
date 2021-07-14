cf4972b4c2b10f8d935f3e50fbed23ab731a3e00
Elias.Ladenburger <elias@ladenburger-family.de>
2021-07-14 09:27:39 +0200

    Update class diagram and goals

--- source/web/app_factory.py
+++ source/web/app_factory.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from globalconfig import config
+from web.api_blueprint import api_bp
 from web.game_blueprint import game_gp
 from web.index_blueprint import index_gp
 from web.scenario_blueprint import scenario_bp
@@ -18,6 +19,7 @@ class AppFactory:
         new_app.register_blueprint(index_gp)
         new_app.register_blueprint(game_gp)
         new_app.register_blueprint(scenario_bp)
+        new_app.register_blueprint(api_bp)
 
         new_app.url_map.strict_slashes = False
 

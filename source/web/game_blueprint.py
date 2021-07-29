21ff0cb2eb95a4be7e859df2796b9bc6d92081cb
Elias.Ladenburger <elias@ladenburger-family.de>
2021-07-29 22:33:56 +0200

    - Reference injects by slug instead of id

--- source/web/game_blueprint.py
+++ source/web/game_blueprint.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 
 from domain.game_play.mock_interface import MockGameProvider
-from domain.scenario_design.injects import InjectType
 
 game_provider = MockGameProvider()
 game = game_provider.get_branching_game()
@@ -34,14 +33,14 @@ def game_start(game_id):
     template_name = "game_start.html"
     first_inject = game.start_game()
     return render_template(template_name, scenario=game.scenario, scenario_name=game.name,
-                           game_id=game_id, inject_id=first_inject.id)
+                           game_id=game_id, inject_slug=first_inject.slug)
 
 
-@game_gp.route("/<game_id>/injects/<inject_id>/stats")
-def stats_page(game_id, inject_id):
+@game_gp.route("/<game_id>/injects/<inject_slug>/stats")
+def stats_page(game_id, inject_slug):
     template_name = "stats_page.html"
     return render_template(template_name, scenario_name=game.name, game_id=game_id,
-                           inject_id=inject_id, game_variables=game.get_visible_vars())
+                           inject_slug=inject_slug, game_variables=game.get_visible_vars())
 
 
 @game_gp.route("/<game_id>/end")
@@ -51,46 +50,36 @@ def game_end(game_id):
 
 
 @game_gp.route("/<game_id>/reflection")
-def game_reflection(game_id: int):
+def game_reflection(game_id):
     template_name = "game_reflection.html"
     game.end_game()
     return render_template(template_name, scenario_name=game.name, game_id=game_id)
 
 
-@game_gp.route("/<game_id>/injects/<inject_id>/show")
-def inject_page(game_id: int, inject_id: int):
-    inject = game.get_inject_by_slug(inject_id=int(inject_id))
-    if inject.type == InjectType.SIMPLE:
-        transitions = []
-    else:
-        transitions = inject.transitions
-    if not transitions or len(transitions) < 2:
-        template_name = "informative_inject.html"
-    else:
+@game_gp.route("/<game_id>/injects/<inject_slug>/show")
+def inject_page(game_id, inject_slug):
+    inject = game.get_inject_by_slug(inject_slug=inject_slug)
+    if inject.has_choices:
         template_name = "choice_inject.html"
+    else:
+        template_name = "informative_inject.html"
     return render_template(template_name, scenario_name=game.name, game_id=game_id,
-                           inject=inject, transitions=transitions)
+                           inject=inject)
 
 
-@game_gp.route("/<game_id>/injects/<inject_id>/solution")
-def solution_page(game_id: int, inject_id: int):
+@game_gp.route("/<game_id>/injects/<inject_slug>/solution")
+def solution_page(game_id, inject_slug):
     solution = request.args.get("solution", 0)
-    next_inject = game.solve_inject(inject=inject_id, solution=int(solution))
+    next_inject = game.solve_inject(inject_candidate=inject_slug, solution=solution)
     if next_inject:
-        return inject_feedback(game_id=game_id, inject_id=next_inject.id)
+        return inject_feedback(game_id=game_id, inject_slug=next_inject.slug)
     else:
         return game_end(game_id)
 
 
-@game_gp.route("/<game_id>/injects/<inject_id>/feedback")
-def inject_feedback(game_id: int, inject_id: int):
+@game_gp.route("/<game_id>/injects/<inject_slug>/feedback")
+def inject_feedback(game_id, inject_slug):
     template_name = "feedback_statistics.html"
     return render_template(template_name, scenario_name=game.name,  game_id=game_id,
-                           next_inject_id=inject_id)
+                           next_inject_slug=inject_slug)
 
-
-@game_gp.route("/<game_id>/injects/<inject_id>/feedback_wordcloud")
-def inject_wordcloud(game_id: int, inject_id: int):
-    template_name = "feedback_wordcloud.html"
-    return render_template(template_name, scenario_name="Going Phishing",  game_id=game_id,
-                           next_inject_id=inject_id)

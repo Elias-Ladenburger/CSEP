7ae719a8d96beaa5772d21ad31c51c8ad260dab7
Elias.Ladenburger <elias@ladenburger-family.de>
2021-08-14 14:32:37 +0200

    Start implementation on facilitate game view

--- source/tests/features/steps/facilitate_group_game.py
+++ source/tests/features/steps/facilitate_group_game.py
@@ -3,6 +3,6 @@ from behave import *
 use_step_matcher("re")
 
 
-@step("the trainer sees all details of the game")
+@step("the trainer sees all details of the gameplay")
 def step_impl(context):
-    raise NotImplementedError(u'STEP: And the trainer sees all details of the game')
+    raise NotImplementedError(u'STEP: And the trainer sees all details of the gameplay')

ca12bc23ccf2933b379ebe5c79dd751a3a41d6db
Elias Ladenburger <elias@ladenburger-family.de>
2021-09-24 14:55:25 +0200

    Make os independent

--- source/presentation_layer/production_server.py
+++ source/presentation_layer/production_server.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+import sys
+import os
+dirname = os.path.dirname(__file__)
+source_dir = os.path.dirname(dirname)
+sys.path.append(source_dir)
+
 from waitress import serve
 
 from presentation_layer.app_factory import AppFactory
@@ -6,4 +13,4 @@ app = AppFactory.create_app()
 
 
 if __name__ == "__main__":
-    serve(app, listen='*:80')
+    serve(app, listen='*:8080')

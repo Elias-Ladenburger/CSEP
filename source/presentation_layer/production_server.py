f719f08d183026b769f2928e0bd03db9510b15db
Elias Ladenburger <elias@ladenburger-family.de>
2021-09-29 15:18:03 +0200

    Hotfix: Enable hot reload for production server

--- source/presentation_layer/production_server.py
+++ source/presentation_layer/production_server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 import os
+
+import werkzeug
+
 dirname = os.path.dirname(__file__)
 source_dir = os.path.dirname(dirname)
 sys.path.append(source_dir)
@@ -12,5 +15,11 @@ from presentation_layer.app_factory import AppFactory
 app = AppFactory.create_app()
 
 
-if __name__ == "__main__":
+@werkzeug.serving.run_with_reloader
+def run_server():
+    app.debug = True # while in development - enables hot reload
     serve(app, listen='*:8080')
+
+
+if __name__ == "__main__":
+    run_server()
